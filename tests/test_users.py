616b1e26836398417acb0ccb07e6dedf53faff85
David Ojeda <david.ojeda@gmail.com>
2019-03-20 12:08:27 +0100

    Fix basic unit tests: models and users

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,9 +2,13 @@ from quetzal.app.models import User
 from quetzal.app.cli.users import user_create
 
 
-def test_new_user(db, db_session):
+def test_new_user(app, db_session):
     """new_user function success"""
+    username = 'username'
     email = 'username@example.com'
-    user_obj = user_create('username', email, 'password')
+    runner = app.test_cli_runner()
+    result = runner.invoke(user_create, [username, email],
+                           input='password\npassword\n')
+    assert not result.exception
     db_obj = User.query.filter_by(email=email).first()
-    assert user_obj == db_obj
+    assert db_obj.username == 'username' and db_obj.email == email
