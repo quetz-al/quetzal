e095da5891fc8073f9785b3a56fc9e8885c54ccb
David Ojeda <david.ojeda@gmail.com>
2019-10-15 19:35:23 +0200

    Add redaction of api key on the gdpr middleware

--- quetzal/app/middleware/gdpr.py
+++ quetzal/app/middleware/gdpr.py
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 def gdpr_log_request():
     from flask import request
-    from quetzal.app.models import User
+    from quetzal.app.models import ApiKey, User
 
     # Quit early if the logging level is not low enough
     if logger.getEffectiveLevel() > logging.DEBUG:
@@ -50,6 +50,19 @@ def gdpr_log_request():
             log_entry['environment']['HTTP_AUTHORIZATION'] = f'{auth_type} ...REDACTED...'
         except:
             pass
+
+    if 'X-Api-Key' in request.headers:
+        key = request.headers['X-Api-Key']
+        log_entry['headers']['X-Api-Key'] = f'...REDACTED...'
+        try:
+            api_key = ApiKey.check_key(key)
+            if api_key is not None:
+                user = api_key.user
+        except:
+            logger.debug('Could not determine user', exc_info=True)
+
+    if 'HTTP_X_API_KEY' in request.environ:
+        log_entry['environment']['HTTP_X_API_KEY'] = f'...REDACTED...'
 
     if user is not None:
         log_entry['user'] = user.username
