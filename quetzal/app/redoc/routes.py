3a14e50136b8345a5f60291be115a0d9b5193afb
David Ojeda <david.ojeda@gmail.com>
2020-03-24 15:40:35 +0100

    Change sole url_for that was using external and https

--- quetzal/app/redoc/routes.py
+++ quetzal/app/redoc/routes.py
@@ -5,7 +5,7 @@ from quetzal.app.redoc import bp
 
 @bp.route('/redoc', methods=['GET'])
 def redoc():
-    spec_url = url_for('/api/v1./api/v1_openapi_json', _external=True, _scheme='https')
+    spec_url = url_for('/api/v1./api/v1_openapi_json')
     return render_template('redoc/index.html',
                            title='Quetzal API - ReDoc',
                            spec_url=spec_url)
